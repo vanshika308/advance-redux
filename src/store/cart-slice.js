ac5cefb745f38097342ee746af19c3681d24a270
vanshika308 <vanshikatiwari0308@gmail.com>
2023-09-12 04:21:58 +0530

    shopping page using reducers

---
+++ src/store/cart-slice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+    name: 'cart',
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+    },
+    reducers:{
+        addItemToCart(state,action){
+          const newItem = action.payload;
+          const existingItem = state.items.find((item)=>item.id===newItem.id)
+          if(!existingItem){
+            state.items.push({
+                itemId: newItem.id,
+                price: newItem.price,
+                quantity : 1,
+                name: newItem.title
+            });
+          }else{
+            existingItem.quantity= existingItem.quantity+1;           
+          }
+        },
+        removeItemTocart(){}
+    }
+})
