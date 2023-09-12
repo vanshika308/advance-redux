f043291de17b195985068b632d5a11104dad1ae3
vanshika308 <vanshikatiwari0308@gmail.com>
2023-09-13 01:06:12 +0530

    add to cart fixes

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,40 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+
+  const DUMMY_PRODUCTS =[
+    {
+      id: 'p1',
+      price: 20,
+      title: 'My First Book',
+      description: 'The first Book I ever wrote'
+    },{
+      id: 'p2',
+      price: 45,
+      title: 'My second Book',
+      description: 'The second Book I ever wrote'
+    },
+    {
+      id: 'p3',
+      price: 40,
+      title: 'My second Book',
+      description: 'The second Book I ever wrote'
+    },
+  ]
+
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map(((products)=>
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+          key={products.id}
+          id={products.id}
+          title={products.title}
+          price={products.price}
+          description={products.description}
+        />))}
       </ul>
     </section>
   );
